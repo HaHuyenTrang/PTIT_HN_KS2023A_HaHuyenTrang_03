3ddd133e375abc828ccd2b077119434828855eb8
HaHuyenTrang <147359400+HaHuyenTrang@users.noreply.github.com>
2025-03-19 17:12:22 +0700

    Add files via upload

---
+++ Exam_Console/src/ra/entity/IApp.java
@@ -0,0 +1,9 @@
+package ra.entity;
+
+import java.util.Scanner;
+
+public interface IApp {
+    public float BASE_SALARY = 1.8f;
+    public void  inputData(Scanner sc);
+    public void  displayData();
+}
